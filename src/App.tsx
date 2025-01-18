dce0a80ff98f1b062317fd6012ce2e7c17a5cc3b
cordelia guims <cordeliaguims@yahoo.com>
2025-01-19 00:11:27 +0100

    implementation logo+title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { AboutSection } from './section/AboutSection';
-import './App.css';
+import React from "react";
+import { AboutSection } from "./section/AboutSection";
+import "./App.css";
+import HeroSection from "./section/HeroSection";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
     <>
+   
+      <HeroSection />
       <AboutSection />
     </>
   );
---
+++ src/section/HeroSection.tsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+  return (
+    <>
+      <div className="relative min-h-screen font-biryani bg-orangeC flex items-center justify-center flex-col z-20">
+        <img
+          src="/public/assets/logo-png.png"
+          alt="logo hiro"
+          className="w-[760px] h-[272px]"
+        />
+        <div className="flex justify-center mt-5 ml-20">
+          <h1 className="text-4xl  font-normal text-whiteC flex ">
+            Hirotechnologies is shaping the <br />
+            future of recycling with a focus on{" "}
+            <div className="ml-10 ">
+              <ul className="text-3xl relative top-12 ">
+                <li>ECOLOGY</li>
+                <li>INNOCATION</li>
+                <li>FUNGI</li>
+              </ul>
+            </div>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
